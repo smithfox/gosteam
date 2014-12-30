0045be784d370de412774048030b6c1f9a1a23ff
smith <smithfox@qq.com>
2014-12-30 19:13:03 +0800

    enhance cookie

--- community/community.go
+++ community/community.go
@@ -1,6 +1,7 @@
 package community
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
@@ -8,29 +9,36 @@ import (
 
 func SetCookies(client *http.Client, sessionId, steamLogin, steamLoginSecure string) {
 	if client.Jar == nil {
-		client.Jar, _ = cookiejar.New(new(cookiejar.Options))
+		client.Jar, _ = cookiejar.New(nil)
 	}
-	base, err := url.Parse("steamcommunity.com")
+	base, err := url.Parse("http://steamcommunity.com")
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("base.host=%s,scheme=%v\n", base.Host, base.Scheme)
 	cookies := []*http.Cookie{
 		// It seems that, for some reason, Steam tries to URL-decode the cookie.
 		&http.Cookie{
-			Name:  "sessionid",
-			Value: url.QueryEscape(sessionId),
+			Name:   "sessionid",
+			Value:  url.QueryEscape(sessionId),
+			Path:   "/",
+			Domain: "steamcommunity.com",
 		},
 		// steamLogin is already URL-encoded.
 		&http.Cookie{
-			Name:  "steamLogin",
-			Value: url.QueryEscape(steamLogin),
+			Name:   "steamLogin",
+			Value:  url.QueryEscape(steamLogin),
+			Path:   "/",
+			Domain: "steamcommunity.com",
 		},
 	}
 
 	if steamLoginSecure != "" {
 		cookies = append(cookies, &http.Cookie{
-			Name:  "steamLoginSecure",
-			Value: url.QueryEscape(steamLoginSecure),
+			Name:   "steamLoginSecure",
+			Value:  url.QueryEscape(steamLoginSecure),
+			Path:   "/",
+			Domain: "steamcommunity.com",
 		})
 	}
 	client.Jar.SetCookies(base, cookies)
@@ -38,30 +46,38 @@ func SetCookies(client *http.Client, sessionId, steamLogin, steamLoginSecure str
 
 func SetCookiesHttps(client *http.Client, sessionId, steamLogin, steamLoginSecure string) {
 	if client.Jar == nil {
-		client.Jar, _ = cookiejar.New(new(cookiejar.Options))
+		client.Jar, _ = cookiejar.New(nil)
 	}
 
-	base, err := url.Parse("steamcommunity.com")
+	base, err := url.Parse("https://steamcommunity.com")
 	if err != nil {
 		panic(err)
 	}
 
+	dddd := client.Jar.Cookies(base)
+	fmt.Printf("base.host=%s,scheme=%v,len(dddd)=%d\n", base.Host, base.Scheme, len(dddd))
 	client.Jar.SetCookies(base, []*http.Cookie{
 		// It seems that, for some reason, Steam tries to URL-decode the cookie.
 		&http.Cookie{
 			Name:   "sessionid",
 			Value:  url.QueryEscape(sessionId),
+			Path:   "/",
+			Domain: "steamcommunity.com",
 			Secure: true,
 		},
 		// steamLogin is already URL-encoded.
 		&http.Cookie{
 			Name:   "steamLogin",
 			Value:  url.QueryEscape(steamLogin),
+			Path:   "/",
+			Domain: "steamcommunity.com",
 			Secure: true,
 		},
 		&http.Cookie{
 			Name:   "steamLoginSecure",
 			Value:  url.QueryEscape(steamLoginSecure),
+			Path:   "/",
+			Domain: "steamcommunity.com",
 			Secure: true,
 		},
 	})
